72e1f40a301e3089529a4fc0fb9ffa32f8299355
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2022-12-30 16:51:15 +0530

    created server

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import server from './server';
+
+server.listen(3001, ()=> {
+    console.log("hello on http://localhost:3001")
+})
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+app.get('/',(req,res)=>{
+    console.log("hello world")
+    res.status(200)
+    res.json(
+        {"message":"hello world"}
+    )
+})
+
+export default app;
