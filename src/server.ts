1b20431858efc1df6817c6115238458729c0bc34
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2022-12-30 23:58:48 +0530

    added jwt auth and added a middleware in router

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
+import {protect} from '../modules/auth'
 
 const app = express();
 
@@ -19,6 +20,6 @@ app.get('/',(req,res)=>{
     )
 })
 
-app.use('/api', router)
+app.use('/api',protect, router)
 
 export default app;
